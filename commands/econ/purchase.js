599ff7e7c7e82efba4e78b235db7e570acc071e7
joeyolson18 <olsonjoey83@gmail.com>
2023-08-07 16:16:06 -0600

    can now buy tokens from the shop

--- commands/econ/purchase.js
+++ commands/econ/purchase.js
@@ -4,9 +4,33 @@ const { purchaseToken } = require('./../../internals/econ');
 module.exports = {
 	data: new SlashCommandBuilder()
 		.setName('purchase')
-		.setDescription('Displays your currency and token balance'),
-
+		.setDescription('Displays your currency and token balance')
+		.addIntegerOption(option =>
+			option.setName('amount')
+				.setDescription('Number of tokens you wish to buy')
+				.setRequired(true),
+		)
+		.addStringOption(option =>
+			option.setName('currency')
+				.addChoices(
+					{ name: 'GPT', value: 'GPT_TOKENS' },
+					{ name: 'dallE', value: 'DALLE_TOKENS' },
+				)
+				.setDescription('Type of token you wish to buy')
+				.setRequired(true),
+		),
 	async execute(interaction) {
-		purchaseToken(interaction.member.user.id);
+		const userId = interaction.member.user.id;
+		const tokenString = interaction.options.getString('currency');
+		const tokenQuantity = interaction.options.getInteger('amount');
+
+		try {
+			const purchaseReply = await purchaseToken(userId, tokenString, tokenQuantity);
+			interaction.reply(purchaseReply);
+		}
+		catch (err) {
+			console.log(err);
+			interaction.reply('Something went wrong!');
+		}
 	},
 };
